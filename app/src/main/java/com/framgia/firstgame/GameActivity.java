fc3df5c51ae7f4204ae5e28b7b66e8912bb94def
Hoang Cong Minh <hoang.cong.minh@framgia.com>
2015-04-27 18:25:54 +0700

    init

---
+++ app/src/main/java/com/framgia/firstgame/GameActivity.java
@@ -0,0 +1,174 @@
+package com.framgia.firstgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+//import android.view.Menu;
+//import android.view.MenuItem;
+
+
+public class GameActivity extends Activity implements View.OnClickListener {
+
+    int correctAnswer;
+    Button buttonObjectChoice1;
+    Button buttonObjectChoice2;
+    Button buttonObjectChoice3;
+    TextView textObjectPartA;
+    TextView textObjectPartB;
+    TextView textObjectScore;
+    TextView textObjectLevel;
+    int currentScore = 0;
+    int currentLevel = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        /*Here we get a working object based on either the button
+        or TextView class and base as well as link our new objects
+        directly to the appropriate UI elements that we created
+        previously*/
+        textObjectPartA   = (TextView)findViewById(R.id.textPartA);
+        textObjectPartB   = (TextView)findViewById(R.id.textPartB);
+
+        textObjectScore = (TextView)findViewById(R.id.textScore);
+        textObjectLevel = (TextView)findViewById(R.id.textLevel);
+
+        buttonObjectChoice1 = (Button)findViewById(R.id.buttonChoice1);
+        buttonObjectChoice2 = (Button)findViewById(R.id.buttonChoice2);
+        buttonObjectChoice3 = (Button)findViewById(R.id.buttonChoice3);
+
+        /*Now we use the setText method of the class on our objects
+        to show our variable values on the UI elements.
+        Just like when we output to the console in the exercise -
+        Expressions in Java, only now we use setText method
+        to put the values in our variables onto the actual UI.*/
+
+        buttonObjectChoice1.setOnClickListener(this);
+        buttonObjectChoice2.setOnClickListener(this);
+        buttonObjectChoice3.setOnClickListener(this);
+
+        setQuestion();
+
+    }//onCreate ends here
+
+    @Override
+    public void onClick(View v) {
+        //declare a new int to be used in all the cases
+        int answerGiven;
+        switch (v.getId()){
+            case R.id.buttonChoice1:
+                //initialize a new int with the value contained in buttonObjectChoice1
+                //Remember we put it there ourselves previously
+                answerGiven = Integer.parseInt("" + buttonObjectChoice1.getText());
+                break;
+            case R.id.buttonChoice2:
+                //same as previous case but using the next button
+                answerGiven = Integer.parseInt("" + buttonObjectChoice2.getText());
+                break;
+            case R.id.buttonChoice3:
+                //same as previous case but using the next button
+                answerGiven = Integer.parseInt("" + buttonObjectChoice3.getText());
+                break;
+        }
+        updateScoreAndLevel(answerGiven);
+        setQuestion();
+    }
+
+    void setQuestion{
+        //generate the parts of the question
+        int numberRange = currentLevel * 3;
+        Random randInt = new Random();
+
+        int partA = randInt.nextInt(numberRange);
+        partA++;//don't want a zero value
+
+        int partB = randInt.nextInt(numberRange);
+        partB++;//don't want a zero value
+
+        correctAnswer = partA * partB;
+        int wrongAnswer1 = correctAnswer-2;
+        int wrongAnswer2 = correctAnswer+2;
+
+        textObjectPartA.setText(""+partA);
+        textObjectPartB.setText(""+partB);
+
+        //set the multi choice buttons
+        //A number between 0 and 2
+        int buttonLayout = randInt.nextInt(3);
+        switch (buttonLayout){
+            case 0:
+                buttonObjectChoice1.setText(""+correctAnswer);
+                buttonObjectChoice2.setText(""+wrongAnswer1);
+                buttonObjectChoice3.setText(""+wrongAnswer2);
+                break;
+            case 1:
+                buttonObjectChoice1.setText(""+wrongAnswer2);
+                buttonObjectChoice2.setText(""+correctAnswer);
+                buttonObjectChoice3.setText(""+wrongAnswer1);
+                break;
+            case 2:
+                buttonObjectChoice1.setText(""+wrongAnswer1);
+                buttonObjectChoice2.setText(""+wrongAnswer2);
+                buttonObjectChoice3.setText(""+correctAnswer);
+                break;
+        }
+    }
+
+    void updateScoreAndLevel(int answerGiven){
+        if(isCorrect(answerGiven)){
+            for(int i = 1; i <= currentLevel; i++){
+                currentScore = currentScore + i;
+            }
+            currentLevel++;
+        }else{
+            currentScore = 0;
+            currentLevel = 1;
+        }
+        //Actually update the two TextViews
+        textObjectScore.setText("Score: " + currentScore);
+        textObjectLevel.setText("Level: " + currentLevel);
+    }
+
+    boolean isCorrect(int answerGiven){
+        boolean correctTrueOrFalse;
+        if(answerGiven == correctAnswer){//YAY!
+            Toast.makeText(getApplicationContext(), "Well done!", Toast.LENGTH_LONG).show();
+            correctTrueOrFalse=true;
+        }else{//Uh-oh!
+            Toast.makeText(getApplicationContext(), "Sorry", Toast.LENGTH_LONG).show();
+            correctTrueOrFalse=false;
+        }
+        return correctTrueOrFalse;
+    }
+
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    */
+}
